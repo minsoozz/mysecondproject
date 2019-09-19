197e9e8a4600de078e65708801c48696e37eff61
hyeongjunmin <alsgudwns007@naver.com>
2019-09-19 16:55:45 +0900

    rhymes

---
+++ src/main/java/com/rhymes/RhymesApplication.java
@@ -0,0 +1,13 @@
+package com.rhymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhymesApplication.class, args);
+	}
+
+}
