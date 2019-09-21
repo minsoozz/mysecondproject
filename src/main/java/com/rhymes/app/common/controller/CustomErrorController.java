84ce1b2ffd383ddc7747230529744b9d0c787100
hyeongjunmin <alsgudwns007@naver.com>
2019-09-21 16:39:34 +0900

    + 타일즈 컨픽 경로 수정

--- src/main/java/com/rhymes/app/common/controller/CustomErrorController.java
+++ src/main/java/com/rhymes/app/common/controller/CustomErrorController.java
@@ -7,9 +7,6 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
 public class CustomErrorController implements ErrorController {
 
@@ -22,12 +19,9 @@ public class CustomErrorController implements ErrorController {
 	}
 	
 	@RequestMapping(value = PATH)
-	public String Error(HttpServletRequest req) {
-		
-		String status = req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) + "";
-		log.info("Error !!!!!!!!!!!!!! info:" + status);
-		
-		switch(status) {
+	public String Error(HttpServletRequest req) {		
+		//에러코드에 맞는 뷰 리턴		
+		switch( (req.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) + "") ) {
 			case "403" : return "errors/403"; 
 			case "404" : return "errors/404";
 			default : return "errors/404";
