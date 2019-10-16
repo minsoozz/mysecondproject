4f9c33328b93ce8c0dfb3912a67c097831f40129
YubSujin <dkvkxm222@naver.com>
2019-10-16 17:45:16 +0900

    @ResponseBody import다시 수정

--- src/main/java/com/rhymes/app/member/controller/AdminMemberController.java
+++ src/main/java/com/rhymes/app/member/controller/AdminMemberController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rhymes.app.member.model.MemBean;
 import com.rhymes.app.member.model.MemberParam;
