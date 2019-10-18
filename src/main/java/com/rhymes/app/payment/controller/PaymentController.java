0ad0cbbd7ed67b2b9c38296873323429086380c1
onep577 <onep577@naver.com>
2019-10-18 17:59:27 +0900

    결제

--- src/main/java/com/rhymes/app/payment/controller/PaymentController.java
+++ src/main/java/com/rhymes/app/payment/controller/PaymentController.java
@@ -20,6 +20,7 @@ import com.rhymes.app.payment.model.OrderDTO;
 import com.rhymes.app.payment.model.PaymentAfDTO;
 import com.rhymes.app.payment.model.PaymentDTO;
 import com.rhymes.app.payment.model.PaymentDetailsDTO;
+import com.rhymes.app.payment.model.PaymentParamDTO;
 import com.rhymes.app.payment.service.PaymentService;
 import com.rhymes.app.payment.util.Coolsms;
 import com.rhymes.app.payment.util.PaymentEmail;
@@ -39,28 +40,35 @@ public class PaymentController {
 		
 		String userid = pcp.getName();
 
+		OrderDTO order_dto = new OrderDTO();
+		order_dto.setStock_seq(Integer.parseInt(stock_seq));
+
 		List<OrderDTO> basketList = new ArrayList<OrderDTO>();
-
-		OrderDTO dto = new OrderDTO();
-		dto.setStock_seq(Integer.parseInt(stock_seq));
-
-		// db 가져오기
-		basketList.add(PaymentService.getOrder(dto));
+		
+		// db 상품 정보 가져오기
+		basketList.add(PaymentService.getOrder(order_dto));
 		
 		// db에는 재고수량이 있고 주문수량은 없다 매개변수로 받은 주문수량을 직접 넣는다
 		basketList.get(0).setQuantity(Integer.parseInt(p_quantity));
+
+		// 리스트에 구매회원 id를 직접 넣는다
 		basketList.get(0).setId(pcp.getName());
+
+		
 		
 		// 총금액 계산
-		String product_price = basketList.get(0).getP_price() * basketList.get(0).getQuantity() + "";
+		int product_price = basketList.get(0).getP_price() * basketList.get(0).getQuantity();
 
+		// 총금액이 10,000원 미만이면 배송비 = 3,000원이다
 		int delivery_price = 0;
-		if(Integer.parseInt(product_price) < 10000) {
+		if(product_price < 10000) {
 			delivery_price = 3000;
 		}
 		
 		
-		// DB 적립금 가져오기
+
+		// DB 적립금, 쿠폰 개수 가져오기
+		// int point_amount = PaymentService.getPointAndCountCoupon(userid);
 		int point_amount = PaymentService.getPoint(userid);
 		
 		// DB 쿠폰 개수 가져오기
@@ -71,24 +79,19 @@ public class PaymentController {
 
 		// 장바구니 내역 지울 수 있는 변수
 		int basket_del = 0;
+
 		
+		
+		// 총 상품 금액, 유효 쿠폰 개수, 유효 적립금, 배송비, 장바구니로 왔는니 구분자
+		PaymentParamDTO param_dto = new PaymentParamDTO(product_price, coupon_count, point_amount, delivery_price, basket_del);
 
-		model.addAttribute("basket_del", basket_del);
+		
+		
+		model.addAttribute("param_dto", param_dto);
 		model.addAttribute("coupon_code", coupon_code);
-		model.addAttribute("point_amount", point_amount);
-		model.addAttribute("coupon_count", coupon_count);
 		model.addAttribute("basketList", basketList);
-		model.addAttribute("product_price", product_price);
-		model.addAttribute("delivery_price", delivery_price);
 
-		if (true) {
-			// 로그인 되어있으면 결제 페이지로 이동
-			return "/payment/payment";
-		} else {
-			// 로그인 안되어있으면 로그인창으로 이동
-			return "/payment/nomembership";
-		}
-
+		return "/payment/payment";
 	}
 
 	// 장바구니 리스트 구매
@@ -98,47 +101,52 @@ public class PaymentController {
 		String userid = pcp.getName();
 		// 장바구니에서 문자열로 데이터를 가져왔다
 		// 예) 신발/청바지/티셔츠			// blist_stockseq : 주문한 상품의 재고번호
-		// 예) 100/80/95				// blist_pQuantity : 주문한 상품의 재고수량
+		// 예) 2/5/1					// blist_pQuantity : 주문 수량
 		
 
 		
 		// 재고번호와 수량을 구분자인 /를 기준으로 배열로 바꿔서 리스트에 넣기
 		// 매개변수로 받은 데이터를 /를 구분자로 자르면 데이터의 개수가 나온다
-		String[] _sqArr = blist_stockseq.split("/");
-		int[] sqArr = Arrays.stream(_sqArr).mapToInt(Integer::parseInt).toArray();
-		String[] _pqArr = blist_pQuantity.split("/");
-		int[] pqArr = Arrays.stream(_pqArr).mapToInt(Integer::parseInt).toArray();
-
-		List<OrderDTO> bOlist = new ArrayList<OrderDTO>();
-
-		for (int i = 0; i < sqArr.length; i++) {
-			OrderDTO order = new OrderDTO();
-
-			order.setStock_seq(sqArr[i]);
-			order.setQuantity(pqArr[i]);
-			bOlist.add(order);
-		}
-
+		String[] _stockseq_arr = blist_stockseq.split("/");
+		int[] stockseq_arr = Arrays.stream(_stockseq_arr).mapToInt(Integer::parseInt).toArray();
+		String[] _pQuantity_arr = blist_pQuantity.split("/");
+		int[] pQuantity_arr = Arrays.stream(_pQuantity_arr).mapToInt(Integer::parseInt).toArray();
 
 		
-		// 장바구니로 데이터를 가져갈 리스트 생성
-		List<OrderDTO> basketList = new ArrayList<OrderDTO>();
 		
-		// bOlist 리스트의 사이즈만큼 즉, 데이터의 개수만큼 for문을 돌린다
+		// 장바구니에서 결제페이지로 데이터를 가져갈 리스트
+		// sqArr의 길이만큼 즉, 데이터의 개수만큼 for문을 돌린다
 		// 예) 신발/청바지/티셔츠			라면 데이터의 개수는 3이다
-		for (int i=0; i<bOlist.size(); i++) {
-			OrderDTO dto = new OrderDTO();
+		List<OrderDTO> basketList = new ArrayList<OrderDTO>();
+
+		for (int i = 0; i < stockseq_arr.length; i++) {
+			OrderDTO order_dto = new OrderDTO();
+		
 			// 데이터를 가져오기 위해서 주문한 상품의 재고번호를 dto에 담고 매개변수로 DB에 보낸다
-			dto.setStock_seq(bOlist.get(i).getStock_seq());
+			order_dto.setStock_seq(stockseq_arr[i]);
 
 			// DB 주문한 상품의 정보 가져오기
 			// 장바구니로 데이터를 가져갈 리스트에 넣는다
-			basketList.add(PaymentService.getOrder(dto));
+			basketList.add(PaymentService.getOrder(order_dto));
 
 			// db로 받을 수 있는 건 재고수량이다
 			// 주문수량은 매개변수로만 받을 수 있어서 직접 dto에 넣는다
-			basketList.get(i).setQuantity(bOlist.get(i).getQuantity());
+			basketList.get(i).setQuantity(pQuantity_arr[i]);
 			basketList.get(i).setId(userid);
+		}
+			
+
+
+		// 총금액 계산
+		int product_price = 0;
+		for (OrderDTO dto : basketList) {
+			product_price += dto.getP_price() * dto.getQuantity();
+		}
+
+		// 총금액이 10,000원 미만이면 배송비 = 3,000원이다
+		int delivery_price = 0;
+		if(product_price < 10000) {
+			delivery_price = 3000;
 		}
 		
 		
@@ -154,33 +162,19 @@ public class PaymentController {
 		
 		// 장바구니 내역 지울 수 있는 변수
 		int basket_del = 1;
-
-		int product_price = 0;
-		for (OrderDTO dto : basketList) {
-			product_price += dto.getP_price() * dto.getQuantity();
-		}
 		
-		int delivery_price = 0;
-		if(product_price < 10000) {
-			delivery_price = 3000;
-		}
+		
 
-		model.addAttribute("basket_del", basket_del);
+		// 총 상품 금액, 유효 쿠폰 개수, 유효 적립금, 배송비, 장바구니로 왔는니 구분자
+		PaymentParamDTO param_dto = new PaymentParamDTO(product_price, coupon_count, point_amount, delivery_price, basket_del);
+		
+		
+
+		model.addAttribute("param_dto", param_dto);
 		model.addAttribute("coupon_code", coupon_code);
-		model.addAttribute("point_amount", point_amount);
-		model.addAttribute("coupon_count", coupon_count);
 		model.addAttribute("basketList", basketList);
-		model.addAttribute("product_price", product_price);
-		model.addAttribute("delivery_price", delivery_price);
 
-		if (true) {
-			// 로그인 되어있으면 결제 페이지로 이동
-			return "/payment/payment";
-		} else {
-			// 로그인 안되어있으면 로그인창으로 이동
-			return "/payment/nomembership";
-		}
-
+		return "/payment/payment";
 	}
 
 	// 결제 후 결제완료창으로 이동
