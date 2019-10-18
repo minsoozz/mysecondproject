020ce31b53638d22e57b475c07531d41774201c1
YubSujin <dkvkxm222@naver.com>
2019-10-18 15:44:57 +0900

    sns 로그인 버튼 수정

--- src/main/java/com/rhymes/app/admin/common/controller/AdminController.java
+++ src/main/java/com/rhymes/app/admin/common/controller/AdminController.java
@@ -6,14 +6,12 @@ import java.util.Date;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.http.HttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rhymes.app.admin.common.service.AdminCommonService;
-import com.rhymes.app.common.service.CommonService;
 
 import lombok.extern.slf4j.Slf4j;
 
