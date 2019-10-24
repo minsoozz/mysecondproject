4bb8f16be0a889a68ba688850a7204278fc2d4e8
onep577 <onep577@naver.com>
2019-10-24 15:38:22 +0900

    통계

--- src/main/java/com/rhymes/app/admin/chart/controller/AdminChartController.java
+++ src/main/java/com/rhymes/app/admin/chart/controller/AdminChartController.java
@@ -13,28 +13,28 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping(value = "/admin/chart")
 public class AdminChartController {
-	
-	@Autowired
-	AdminChartService AdminChartService;
-	
-	@GetMapping(value = "salary")
-	public String salary() {
-		return "salary.tiles";
-	}
-	
-	@GetMapping(value = "member")
-	public String member() {
-		return "member.tiles";
-	}
-	
-	@GetMapping(value = "category")
-	public String category() {
-		return "category.tiles";
-	}
-	
-	@GetMapping(value = "product")
-	public String product() {
-		return "product.tiles";
-	}
+//	
+//	@Autowired
+//	AdminChartService AdminChartService;
+//	
+//	@GetMapping(value = "salary")
+//	public String salary() {
+//		return "salary.tiles";
+//	}
+//	
+//	@GetMapping(value = "member")
+//	public String member() {
+//		return "member.tiles";
+//	}
+//	
+//	@GetMapping(value = "category")
+//	public String category() {
+//		return "category.tiles";
+//	}
+//	
+//	@GetMapping(value = "product")
+//	public String product() {
+//		return "product.tiles";
+//	}
 
 }
