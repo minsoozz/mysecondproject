4d99906dc6d28362a759da6cfea40951c38af960
onep577 <onep577@naver.com>
2019-11-08 17:03:36 +0900

    차트

--- src/main/resources/static/js/admin/chart/salary_chart.js
+++ src/main/resources/static/js/admin/chart/salary_chart.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
 	//alert("어드민 통계");	
 	
 	$("#_btnSearch").click(function () {
-		alert("클릭");
+		//alert("클릭");
 		
 		var year = $("#_select_year").val();
 		var month = $("#_select_month").val();
-		alert(year + ", " + month);
+		//alert(year + ", " + month);
 		
 		location.href="/admin/chart/salary?year="+year+"&month="+month;
 	});
